21dcaead97a06f0fa259d2be7a48f3b943368b71
alexmaichen <108679492+alexmaichen@users.noreply.github.com>
2024-08-21 20:42:16 +0200

    Add files via upload

---
+++ timeCmp.py
@@ -0,0 +1,67 @@
+from math import floor
+
+def compareTimes(player1: str, player2: str) -> list:
+    time1 = timeToFloat(player1)
+    time2 = timeToFloat(player2)
+    
+    if time1 > time2:
+        return [player2, player1]
+    if time1 < time2:
+        return [player1, player2]
+    # if the two times are equal, return in current order
+    return [player1, player2]
+
+def addTimes(player1: str, player2: str):
+    return floatToTime(timeToFloat(player1) + timeToFloat(player2))
+
+def subTimes(player1: str, player2: str):
+    return floatToTime(timeToFloat(player1) - timeToFloat(player2))
+
+def timeToFloat(player1: str):
+    total = 0
+    time1 = player1.split(":")
+    time1 = [float(i) for i in time1]
+    total += time1.pop()
+    if time1:
+        total += time1.pop() * 60 # seconds -> minutes
+    if time1:
+        total += time1.pop() * 60 * 60 # minutes -> hours
+    if time1:
+        total += time1.pop() * 60 * 60 * 24 # hours -> days
+    return total
+
+def floatToTime(num: float):
+    timer = ""
+    
+    if floor(num/(60*60*24)) < 10:
+        timer = "0" + str(floor(num/(60*60*24))) + ':'
+    else:
+        timer += str(floor(num/(60*60*24))) + ':'
+    if num >= 60*60*24:
+        num %= 24
+    
+    if floor(num/(60*60)) < 10:
+        timer += "0" + str(floor(num/(60*60))) + ':'
+    else:
+        timer += str(floor(num/(60*60))) + ':'
+    if num >= 60*60:
+        num %= 60
+    
+    if floor(num/60) < 10:
+        timer += "0" + str(floor(num/60)) + ':'
+    else:
+        timer += str(floor(num/60)) + ':'
+    if num >= 60:
+        num %= 60
+    
+    if floor(num) < 10:
+        timer += "0" + str(floor(num))
+    else:
+        timer += str(floor(num))
+    
+    return timer
+
+if __name__ == "__main__":
+    print(floatToTime(61))
+    print(timeToFloat("01:01"))
+    print(compareTimes("20:05", "15:19"))
